b12ee8c75b108965c3440d25df0a4da9b0028850
Qugerd <qugerd.main@gmail.com>
2022-05-04 19:39:24 +1000

    commit

---
+++ Zeidel.py
@@ -0,0 +1,37 @@
+import numpy as np
+from massive import A
+
+# [0.68505222 1.03484008 0.56298956 0.01525783]
+k = 0
+n = A.shape[0]
+eps = 10 ** -10
+
+x = np.zeros(n)
+x_0 = np.squeeze(np.asarray(A[:, len(A[0]) - 1:]))
+x_new = x_0.copy()
+mat_c = A.copy()
+
+for i in range(n):
+    for j in range(n + 1):
+        mat_c[i, j] = -A[i, j] / A[i, i]
+        if j == n:
+            mat_c[i, j] *= -1
+
+while max(abs(x - x_new) > eps):
+    k += 1
+    x_0 = x.copy()
+    x_new = x.copy()
+    x = np.zeros(n)
+    for i in range(n):
+        for j in range(n + 1):
+            if j != n:
+                x[i] += mat_c[i, j] * x_0[j]
+            else:
+                x[i] += mat_c[i, j]
+
+        x[i] -= mat_c[i, i] * x_0[i]
+        x_0[i] = x[i]
+
+print(x)
+print(k)
+

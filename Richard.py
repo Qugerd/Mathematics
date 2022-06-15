ae20b4636d1dea9257257468ed241d9cf9a35d1d
Qugerd <qugerd.main@gmail.com>
2022-06-16 00:55:30 +1000

    Update Richard.py

--- Richard.py
+++ Richard.py
@@ -1,18 +1,6 @@
+import math
 import numpy as np
-np.set_printoptions(precision=2, suppress=True)
-
-def Richard(A, f, k):
-    n = len(f)
-    ls = np.linalg.eig(A)[0]
-    a, b = min(ls), max(ls)
-    x = np.array([0.0] * n)
-
-    for i in range(k):
-        t = 2 / ((a + b) + (b - a) * np.cos((2*i - 1) * np.pi / (2 * k)))
-        x += t * (f - np.array(x * A)[0])
-
-    return x
-
+from numpy import linalg as la
 
 A = np.matrix([
     [10.9, 1.2, 2.1, 0.9],
@@ -22,5 +10,31 @@ A = np.matrix([
 ])
 
 b = np.array([-7.0, 5.3, 10.3, 24.6])
+eps = 10 ** -5
 
-print(Richard(A, b, 16))
+def richardson(matrix, vector, maxSelf, minSelf, n):
+    matrix = np.array(matrix)
+    x_prev = np.array([0] * matrix.shape[0])
+    x = np.array([1] * matrix.shape[0])
+    tau = 2 / (minSelf + maxSelf)
+    mu = minSelf / maxSelf
+    p = (1 - mu) / (1 + mu)
+    iteration = 0
+    count = 0
+    while (la.norm(x_prev - x) >= eps):
+        for k in range(1, n + 1):
+            x_prev = x
+            nu = math.cos((2 * k - 1) * math.pi / (2 * n))
+            tau_k = tau / (1 - p * nu)
+            x = (vector - matrix @ x_prev) * tau_k + x_prev
+            iteration += 1
+            print(iteration, 'iter', x)
+        count += 1
+    print(count)
+    return x
+
+
+n = 10
+ls = la.eig(A)[0]
+minSelf, maxSelf = min(ls), max(ls)
+richardson(A, b, maxSelf, minSelf, n)
